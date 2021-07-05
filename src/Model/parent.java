2d8bc5d2a3498739c314dda3d54a04b9d7b502bc
hari98486 <53962864+hari98486@users.noreply.github.com>
2021-07-05 18:38:51 +0530

    Add files via upload

---
+++ src/Model/parent.java
@@ -0,0 +1,39 @@
+package Model;
+
+public class parent {
+	static String name;
+	String username;
+	static String password;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	String user1;
+	static String password1;
+	public String getUser1() {
+		return user1;
+	}
+	public void setUser1(String user1) {
+		this.user1 = user1;
+	}
+	public String getPassword1() {
+		return password1;
+	}
+	public void setPassword1(String password1) {
+		this.password1 = password1;
+	}
+}
